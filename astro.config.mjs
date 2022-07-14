03b2e64246c27e8a8708b9b75c458e71e770371f
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-07-14 16:42:20 -0400

    repo-rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), sitemap()]
+  integrations: [svelte()]
 });
