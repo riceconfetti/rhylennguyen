fe752ed77e537c989741bb791a97d82fd0b446d5
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-08-23 06:56:39 -0400

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,44 @@
-<script>
+<script lang="ts">
 	import '../app.css';
-	import { Menu } from 'lucide-svelte';
-
+	import { Menu, X } from 'lucide-svelte';
 	const pages = [
 		{
 			name: 'about',
 			color: true
 		},
 		{
-			name: 'skills',
-			color: false
-		},
-		{
 			name: 'projects',
 			color: false
 		},
+		{
+			name: 'skills',
+			color: false
+		},
+
 		{
 			name: 'contact',
 			color: true
 		}
 	];
+	$: sidebar = false;
 </script>
 
 <nav
-	class="fixed z-10 w-full flex justify-between items-center font-bold p-4 px-6 md:py-6 md:px-10 border-b border-opacity-15 border-citrine bg-cynical md:-translate-y-full"
+	class="bg-cynical fixed z-30 w-full flex justify-between items-center font-bold p-4 px-6 md:py-6 md:px-10 border-b border-opacity-15 border-citrine md:-translate-y-full"
 >
 	<a href="/">rhylen<span class="text-citrine">.</span>n</a>
-	<Menu class="md:hidden" />
+
+	<button
+		on:click={() => {
+			sidebar = !sidebar;
+		}}
+		class="hamburger md:hidden"
+	>
+		<Menu />
+	</button>
+
+	<!-- <X class="{sidebar ? '' : 'hidden'}    text-cynical" /> -->
+
 	<div class="hidden md:flex gap-6">
 		{#each pages as p}
 			<a href="#{p.name}">{p.name}</a>
@@ -35,5 +47,31 @@
 </nav>
 
 <div class="relative h-full">
+	<div
+		class="{sidebar
+			? ''
+			: 'translate-x-full transition ease-in-out duration-500'} circle-menu transition-all ease-in-out duration-500 font-bold text-3xl text-cynical bg-citrine rounded-full aspect-square flex flex-col gap-20 h-[125%] -top-[13%] fixed z-30 items-star pl-48 justify-center -left-10"
+	>
+		<nav
+			class="fixed top-0 left-0 z-30 w-full flex justify-end items-center font-bold p-4 px-6 md:py-6 md:px-10"
+		>
+			<button
+				on:click={() => {
+					sidebar = !sidebar;
+				}}
+				class="hamburger md:hidden pl-2"
+			>
+				<X />
+			</button>
+		</nav>
+		{#each pages as p}
+			<a
+				href="#{p.name}"
+				on:click={() => {
+					sidebar = !sidebar;
+				}}>{p.name}.</a
+			>
+		{/each}
+	</div>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,202 +1,39 @@
 <script lang="ts">
 	import { gsap } from 'gsap/dist/gsap';
 	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+	import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
 	import { onMount } from 'svelte';
+	import SideHeader from '$lib/components/SideHeader.svelte';
 
 	onMount(() => {
-		gsap.registerPlugin(ScrollTrigger);
+		gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-		let currentIndex = -1;
-		let animating: Boolean;
-		let swipePanels = gsap.utils.toArray('.swipe-section section');
+		const sections = gsap.utils.toArray('.swipe-section section');
+		const headers = gsap.utils.toArray('.swipe-headers hd');
 
-		// set second panel two initial 100%
-		gsap.set('.y-100', { yPercent: 100 });
-
-		// set z-index levels for the swipe panels
-		gsap.set(swipePanels, {
-			zIndex: (i) => i
-		});
-
-		// create an observer and disable it to start
-		let intentObserver = ScrollTrigger.observe({
-			type: 'wheel,touch',
-			onUp: () => !animating && gotoPanel(currentIndex + 1, true),
-			onDown: () => !animating && gotoPanel(currentIndex - 1, false),
-			wheelSpeed: -1,
-			tolerance: 10,
-			preventDefault: true,
-			onPress: (self) => {
-				// on touch devices like iOS, if we want to prevent scrolling, we must call preventDefault() on the touchstart (Observer doesn't do that because that would also prevent side-scrolling which is undesirable in most cases)
-				ScrollTrigger.isTouch && self.event.preventDefault();
-			}
-		});
-		intentObserver.disable();
-
-		// handle the panel swipe animations
-		function gotoPanel(index: number, isScrollingDown: Boolean) {
-			animating = true;
-			// return to normal scroll if we're at the end or back up to the start
-			if ((index === swipePanels.length && isScrollingDown) || (index === -1 && !isScrollingDown)) {
-				let target = index;
-				gsap.to(target, {
-					// yPercent: isScrollingDown ? -100 : 0,
-					duration: 0.0,
-					onComplete: () => {
-						animating = false;
-						isScrollingDown && intentObserver.disable();
-					}
-				});
-				return;
-			}
-
-			//   target the second panel, last panel?
-			let target = isScrollingDown ? swipePanels[index] : swipePanels[currentIndex];
-
-			gsap.to(target, {
-				yPercent: isScrollingDown ? 0 : 100,
-				duration: 0.75,
-				onComplete: () => {
-					animating = false;
-				}
+		sections.forEach((panel: any, i) => {
+			ScrollTrigger.create({
+				trigger: panel,
+				start: 'top top',
+				markers: true,
+				pin: true,
+				pinSpacing: false,
+				snap: 1
 			});
-			currentIndex = index;
-			console.log(index);
-		}
-
-		// pin swipe section and initiate observer
-		ScrollTrigger.create({
-			trigger: '.swipe-section',
-			pin: true,
-			start: 'top top',
-			end: '+=1',
-			onEnter: (self) => {
-				intentObserver.enable();
-				gotoPanel(currentIndex + 1, true);
-			},
-			onEnterBack: () => {
-				intentObserver.enable();
-				gotoPanel(currentIndex - 1, false);
-			}
 		});
 
-		let currentHIndex = -1;
-		let animatingH: Boolean;
-
-		let headers = gsap.utils.toArray('.animateHeader .swipeHeader');
-
-		gsap.set('.x-100', { xPercent: 100 });
-
-		gsap.set(headers, {
-			zIndex: (i) => i
-		});
-
-		let intentHObserver = ScrollTrigger.observe({
-			type: 'wheel,touch',
-			onUp: () => !animatingH && gotoHeader(currentHIndex + 1, true),
-			onDown: () => !animatingH && gotoHeader(currentHIndex - 1, false),
-			wheelSpeed: -1,
-			tolerance: 10,
-			preventDefault: true,
-			onPress: (self) => {
-				// on touch devices like iOS, if we want to prevent scrolling, we must call preventDefault() on the touchstart (Observer doesn't do that because that would also prevent side-scrolling which is undesirable in most cases)
-				ScrollTrigger.isTouch && self.event.preventDefault();
-			}
-		});
-		intentHObserver.disable();
-
-		function gotoHeader(index: number, isScrollingDown: Boolean) {
-			animatingH = true;
-			// return to normal scroll if we're at the end or back up to the start
-			if ((index === headers.length && isScrollingDown) || (index === -1 && !isScrollingDown)) {
-				let target = index;
-				gsap.to(target, {
-					// yPercent: isScrollingDown ? -100 : 0,
-					duration: 0.0,
-					onComplete: () => {
-						animatingH = false;
-						isScrollingDown && intentHObserver.disable();
-					}
-				});
-				return;
-			}
-
-			//   target the second panel, last panel?
-			let target = isScrollingDown ? headers[index] : headers[currentHIndex];
-			let parent = isScrollingDown ? headers[currentHIndex] : headers[currentHIndex - 1];
-
-			gsap.to(target, {
-				xPercent: isScrollingDown ? 0 : 100,
-				duration: 0.75,
-				onComplete: () => {
-					animatingH = false;
-				}
-			});
-
-			gsap.to(parent, {
-				xPercent: isScrollingDown ? 100 : 0,
-				duration: 0.75,
-				onComplete: () => {
-					animatingH = false;
-				}
-			});
-			currentHIndex = index;
-			console.log(index);
-		}
-
-		ScrollTrigger.create({
-			trigger: '.animateHeader',
-			pin: true,
-			start: 'top top',
-			end: '+=1',
-			onEnter: (self) => {
-				intentHObserver.enable();
-				gotoHeader(currentHIndex + 1, true);
-			},
-			onEnterBack: () => {
-				intentHObserver.enable();
-				gotoHeader(currentHIndex - 1, false);
-			}
-		});
+		function animateHeader() {}
 	});
-
-	const pages = [
-		{
-			name: 'home',
-			color: false
-		},
-		{
-			name: 'about',
-			color: true
-		},
-		{
-			name: 'skills',
-			color: false
-		}
-	];
 </script>
 
-<div
-	class="fixed animateHeader size-full flex justify-end items-center z-10 inset-0 pointer-events-none cursor-none"
->
-	<h2
-		class="swipeHeader text-6xl leading-0 font-bold uppercase tracking-[1rem] [writing-mode:sideways-lr]"
-	>
-		home
-	</h2>
-	<h2
-		class="x-100 swipeHeader text-6xl leading-0 font-bold uppercase tracking-[1rem] [writing-mode:sideways-lr] text-cynical"
-	>
-		about
-	</h2>
-	<h2
-		class="x-100 swipeHeader text-6xl leading-0 font-bold uppercase tracking-[1rem] [writing-mode:sideways-lr]"
-	>
-		skills
-	</h2>
-</div>
+<!-- LOADER -->
+<!-- <div class="loader bg-cynical fixed inset-0 flex items-center justify-center z-50">
+	<script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/newtonsCradle.js"></script>
 
-<div class="swipe-section size-full">
+	<l-newtons-cradle size="78" speed="1.4" color="rgb(255 199 0)"></l-newtons-cradle>
+</div> -->
+
+<div class="swipe-section h-screen w-screen">
 	<section id="home" class="size-full flex flex-col justify-center items-center md:p-10 bg-cynical">
 		<div>
 			<h1 class="font-bold text-xl">Hi, I'm <span class="text-citrine">Rhylen</span>,</h1>
@@ -212,7 +49,7 @@
 	</section>
 	<section id="about" class="size-full flex flex-col justify-center items-center bg-cynical y-100">
 		<div
-			class="bg-citrine text-sm text-cynical leading-2 w-full font-bold flex flex-col gap-6 p-16 pr-24 md:p-10"
+			class="bg-citrine text-xs text-cynical leading-2 w-full font-bold flex flex-col gap-6 p-12 pr-24 md:p-10"
 		>
 			<p>
 				Hi, I'm Rhylen and I have a passion for creating novel experiences on the web. I started my
@@ -229,8 +66,28 @@
 			</p>
 		</div>
 	</section>
-	<section id="skill" class="size-full flex flex-col justify-center items-center bg-cynical y-100">
-		<div class="text-sm leading-2 w-full font-bold flex flex-col gap-6 p-16 pr-24 md:p-10">
+	<section
+		id="projects"
+		class="size-full flex flex-col justify-center items-center bg-cynical y-100"
+	>
+		<div class="text-xs leading-2 w-full font-bold flex flex-col gap-6 p-12 pr-24 md:p-10">
+			<p>
+				Hi, I'm Rhylen and I have a passion for creating novel experiences on the web. I started my
+				journey in the digital world when I was 10 and picked up the book "How to build your own
+				website." Since then I've been been around the internet with Greasemonkey userscripts and
+				Tumblr theme customizations and fell in love with programming.
+			</p>
+			<p>
+				These days, I'm still just as passionate about computers as I was at 10 years old. Over the
+				years I've become experienced in a wide variety of web technologies and languages. My main
+				focus is building easy to navigate front-end experiences and making technology intuitive.
+				When I'm not coding, I love dancing, reading, and playing board and table-top role playing
+				games.
+			</p>
+		</div>
+	</section>
+	<section id="skills" class="size-full flex flex-col justify-center items-center bg-cynical y-100">
+		<div class="text-xs leading-2 w-full font-bold flex flex-col gap-6 p-12 pr-24 md:p-10">
 			<p>
 				Hi, I'm Rhylen and I have a passion for creating novel experiences on the web. I started my
 				journey in the digital world when I was 10 and picked up the book "How to build your own
@@ -248,7 +105,9 @@
 	</section>
 </div>
 
-<style>
+<SideHeader class="fixed" />
+
+<style scoped>
 	.swipe-section {
 		position: relative;
 		height: 100vh;
@@ -256,8 +115,7 @@
 		overflow: visible;
 	}
 
-	.swipe-section section,
-	.swipeHeader {
-		position: absolute;
+	.swipe-section section {
+		/* position: absolute; */
 	}
 </style>
