b2c141df3ce7f4be010d62492d70e75844a8015d
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-08-26 21:18:00 -0400

    fallback

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: false
 		})
